ba31764ec73dbeb11edfdda55f5a7b62697c04ec
acquay <acquay707@mail.com>
2025-02-28 07:54:28 -0600

    modified eks-cluster

--- terraform-eks/eks-cluster.tf
+++ terraform-eks/eks-cluster.tf
@@ -5,5 +5,25 @@ module "eks" {
   cluster_name     = "myapp-eks-cluster"
   cluster_version  = "1.27"
 
-  
+  subnet_ids = module.myapp-vpc.private_subnets
+  vpc_id     = module.myapp-vpc.vpc_id 
+
+  tags = {
+    envn = "dev"
+    application = "myapp"
+  }
+
+   eks_managed_node_groups = {
+      dev = {
+      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
+      #ami_type       = "AL2023_x86_64_STANDARD"
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+      instance_types = ["t2_small"]
+
+    }
+   } 
+
+
 }
